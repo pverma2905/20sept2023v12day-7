8f28a252949eaa8c7a5b25d0c27d4fb68c403608
pverma2905 <pverma2905@gmail.com>
2023-09-26 10:52:26 +0530

    xxx

---
+++ helper/constant.js
@@ -0,0 +1,15 @@
+// import area
+
+
+//Defination area
+const TITLE = "Find Businesses Near You on Local Search Engine - Justdial";
+const DESCRIPTION ="Justdial, India's No. 1 local search engine, for Restaurants, Hotels, Salons, Real Estate, Travel, Healthcare, Education, B2B Businesses and more. Find addresses, phone numbers, reviews and ratings, photos, maps of businesses."
+
+
+// export area
+//3.1 default export 
+export default DESCRIPTION;
+
+//3.2 named export
+//export {TITLE}
+module.exports = {TITLE}
---
+++ pages/cms/career-opportunities.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CareerOpportunities() {
+  return (
+    <div>career-opportunities</div>
+  )
+}
